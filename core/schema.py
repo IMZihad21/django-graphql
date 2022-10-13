411f162a0755e868e93bab7922f8db5eedb52ee3
ZèD <imzihad@gmail.com>
2022-10-13 16:02:54 +0600

    graphql: initialized graphql with jwt authentication backend
    Signed-off-by: ZèD <imzihad@gmail.com>

---
+++ core/schema.py
@@ -0,0 +1,35 @@
+import graphene
+import graphql_jwt
+from django.contrib.auth.models import User
+from graphene_django import DjangoObjectType
+from graphql_jwt.decorators import login_required
+
+
+class UserType(DjangoObjectType):
+    class Meta:
+        model = User
+        fields = ("id", "username", "email")
+
+
+class Mutation(graphene.ObjectType):
+    token_auth = graphql_jwt.ObtainJSONWebToken.Field()
+    verify_token = graphql_jwt.Verify.Field()
+    refresh_token = graphql_jwt.Refresh.Field()
+    
+    revoke_token = graphql_jwt.Revoke.Field()
+
+    delete_token_cookie = graphql_jwt.DeleteJSONWebTokenCookie.Field()
+    delete_refresh_token_cookie = graphql_jwt.DeleteRefreshTokenCookie.Field()
+
+
+class Query(graphene.ObjectType):
+    hello = graphene.String(default_value="Hi!")
+
+    viewer = graphene.Field(UserType)
+
+    @login_required
+    def resolve_viewer(self, info, **kwargs):
+        return info.context.user
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
--- core/urls.py
+++ core/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.views.decorators.csrf import csrf_exempt
+from graphene_django.views import GraphQLView
+from graphql_jwt.decorators import jwt_cookie
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", csrf_exempt(jwt_cookie(GraphQLView.as_view(graphiql=True)))),
 ]
